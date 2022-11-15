5e4a67e14c9e184b6817e95c9d92b353478789ee
adelnizamutdinov <stiggpwnz@gmail.com>
2022-11-15 12:52:25 +0300

    remix

---
+++ remix/app/root.tsx
@@ -0,0 +1,36 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react"
+import type { LinksFunction, MetaFunction } from "@remix-run/cloudflare"
+import styles from "./styles/app.css"
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+})
+
+// noinspection JSUnusedGlobalSymbols
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }]
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  )
+}
---
+++ remix/app/routes/index.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function Index() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+}
