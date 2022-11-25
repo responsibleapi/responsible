840d436d98f257eef38ca6f3e2c051ef27f9ced7
adelnizamutdinov <stiggpwnz@gmail.com>
2022-11-25 13:48:14 +0300

    codemirror

--- remix/app/routes/index.tsx
+++ remix/app/routes/index.tsx
@@ -1,69 +1,26 @@
-import { genVertxKotlinClient } from "../../../generator/src/kotlin/vertx-client"
-import { genPythonTypes } from "../../../generator/src/python/dataclasses"
 import { toOpenApi } from "../../../generator/src/openapi/to-open-api"
 import type { CoreService } from "../../../generator/src/core/core"
 import { kdlToCore } from "../../../generator/src/dsl/kdl/kdl"
 import React, { useEffect, useMemo, useState } from "react"
-import { RadioGroup } from "@headlessui/react"
+import { jsonLanguage } from "@codemirror/lang-json"
+import CodeMirror from "@uiw/react-codemirror"
 import { exampleKDL } from "../examplekdl"
-import Editor from "@monaco-editor/react"
 import { parse } from "kdljs"
-import classNames from "clsx"
-
-const formatObj = {
-  openapi: "openapi",
-  "vertx-kotlin": "vertx-kotlin",
-  "python-types": "python-types",
-} as const
-type Format = typeof formatObj[keyof typeof formatObj]
-
-const formatArr = Object.values(formatObj)
-
-const labels: Record<Format, string> = {
-  openapi: "OpenAPI",
-  "vertx-kotlin": "Vert.x Kotlin",
-  "python-types": "Python dataclasses",
-}
-
-const languages: Record<Format, string> = {
-  openapi: "json",
-  "vertx-kotlin": "kotlin",
-  "python-types": "python",
-}
 
 const LOCAL_STORAGE_KEY = "kdl"
-const LOCAL_STORAGE_FMT_KEY = "fmt"
 
 const useSaveLocalStorage = (k: string, v: string): void =>
   useEffect(() => localStorage.setItem(k, v), [k, v])
 
-const render = (fmt: Format, core: CoreService): string => {
-  switch (fmt) {
-    case "python-types":
-      return genPythonTypes(core)
-
-    case "openapi":
-      return JSON.stringify(toOpenApi(core), null, 2) + "\n"
-
-    case "vertx-kotlin":
-      return genVertxKotlinClient(core, {})
-  }
-}
-
 // noinspection JSUnusedGlobalSymbols
 export default function Index(): JSX.Element {
   const [kdl, setKDL] = useState("")
-  const [fmt, setFmt] = useState(formatArr[0])
 
   useEffect(() => {
     setKDL(localStorage.getItem(LOCAL_STORAGE_KEY) ?? exampleKDL)
-    setFmt(
-      (localStorage.getItem(LOCAL_STORAGE_FMT_KEY) as Format) ?? formatArr[0],
-    )
   }, [])
 
   useSaveLocalStorage(LOCAL_STORAGE_KEY, kdl)
-  useSaveLocalStorage(LOCAL_STORAGE_FMT_KEY, fmt)
 
   const core: CoreService = useMemo(() => {
     try {
@@ -73,60 +30,27 @@ export default function Index(): JSX.Element {
     }
   }, [kdl])
 
-  const result: string = useMemo(() => render(fmt, core), [core, fmt])
+  const result: string = useMemo(
+    () => JSON.stringify(toOpenApi(core), null, 2) + "\n",
+    [core],
+  )
 
   return (
     <div className="flex flex-row w-full h-screen divide-x">
-      <div className="flex-1">
-        <Editor
-          language="kdl"
-          value={kdl}
-          options={{ minimap: { enabled: false } }}
-          onChange={str => setKDL(str || "")}
-        />
-      </div>
+      <CodeMirror
+        className="flex-1 h-full"
+        height={"100%"}
+        value={kdl}
+        onChange={str => setKDL(str || "")}
+      />
 
-      <div className="flex flex-col flex-1">
-        <RadioGroup
-          value={fmt}
-          onChange={setFmt}
-          className="mt-2 grid grid-cols-3 gap-3 sm:grid-cols-6"
-        >
-          {formatArr.map(option => (
-            <RadioGroup.Option
-              key={option}
-              value={option}
-              className={({ active, checked }) =>
-                classNames(
-                  "cursor-pointer focus:outline-none border rounded-md py-3 px-3 flex items-center justify-center text-sm font-medium sm:flex-1",
-                  {
-                    "ring-2 ring-offset-2 ring-indigo-500": active,
-
-                    "bg-indigo-600 border-transparent text-white hover:bg-indigo-700":
-                      checked,
-
-                    "bg-white border-gray-200 text-gray-900 hover:bg-gray-50":
-                      !checked,
-                  },
-                )
-              }
-            >
-              <RadioGroup.Label as="span">{labels[option]}</RadioGroup.Label>
-            </RadioGroup.Option>
-          ))}
-        </RadioGroup>
-
-        <div className={"h-full"}>
-          <Editor
-            options={{
-              readOnly: true,
-              minimap: { enabled: false },
-            }}
-            value={result}
-            language={languages[fmt]}
-          />
-        </div>
-      </div>
+      <CodeMirror
+        className="flex-1 h-full"
+        readOnly={true}
+        height={"100%"}
+        value={result}
+        extensions={[jsonLanguage]}
+      />
     </div>
   )
 }
