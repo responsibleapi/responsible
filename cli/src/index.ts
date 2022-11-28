d481d4e42a25c4555c368daef617b23d577be4c0
adelnizamutdinov <stiggpwnz@gmail.com>
2022-11-28 14:06:03 +0300

    --version arg

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,8 +1,10 @@
 import { toOpenApi } from "../../generator/src/openapi/to-open-api"
 import { kdlToCore } from "../../generator/src/dsl/kdl/kdl"
+import packageJson from "../package.json"
 import { readFile } from "fs/promises"
 import * as process from "process"
 import { parse } from "kdljs"
+import arg from "arg"
 
 const die = (s: string): never => {
   console.error(s)
@@ -10,10 +12,18 @@ const die = (s: string): never => {
 }
 
 const main = async () => {
-  const file = process.argv[process.argv.length - 1]
-  if (!file.endsWith(".kdl")) {
-    return die(`expected .kdl file, got ${file}`)
+  const args = arg({
+    "--version": Boolean,
+  })
+
+  if (args["--version"]) {
+    console.log(packageJson.version)
+    return
   }
+
+  const file = args._[0]
+  if (!file) return die("Specify a .kdl file")
+  if (!file.endsWith(".kdl")) return die(`expected .kdl file, got ${file}`)
 
   const kdl = await readFile(file, "utf8")
   const doc = parse(kdl)
