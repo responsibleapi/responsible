25166f93cbb209f8014dbbdfce69f127e675053d
adelnizamutdinov <stiggpwnz@gmail.com>
2023-09-10 03:04:02 +0800

    amen

--- astro/tailwind.config.ts
+++ astro/tailwind.config.ts
@@ -2,7 +2,7 @@ import typo from "@tailwindcss/typography"
 import { type Config } from "tailwindcss"
 
 const cfg: Config = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,jsx,md,mdx,svelte,tsx,vue}"],
   theme: {
     extend: {},
   },
