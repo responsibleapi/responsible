dcfa894361e1758bc71076d2e3017a399700253f
adelnizamutdinov <stiggpwnz@gmail.com>
2024-11-13 02:32:19 +0300

    amen

--- website/tailwind.config.ts
+++ website/tailwind.config.ts
@@ -1,13 +1,10 @@
-import typo from "@tailwindcss/typography"
+import typography from "@tailwindcss/typography"
 import { type Config } from "tailwindcss"
 
-const cfg: Config = {
+export default {
   content: ["./src/**/*.{astro,html,jsx,md,mdx,svelte,tsx,vue}", "index.html"],
   theme: {
     extend: {},
   },
-  plugins: [typo],
-}
-
-// noinspection JSUnusedGlobalSymbols
-export default cfg
+  plugins: [typography],
+} satisfies Config
