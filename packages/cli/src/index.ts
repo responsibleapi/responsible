8aa68e2c54abc26e119b280274db10c9d9760b92
adelnizamutdinov <stiggpwnz@gmail.com>
2025-02-03 05:03:15 +0300

    amen

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -2,7 +2,7 @@ import { Command } from "commander"
 import { parse as parseKDL } from "kdljs"
 import * as fs from "node:fs/promises"
 import { toOpenAPI } from "../../generator/src/kdl"
-import { version } from "../package.json"
+import * as packageJSON from "../package.json"
 
 function die(s: string): never {
   console.error(s)
@@ -26,9 +26,9 @@ const onKdlChange =
     }
   }
 
-const program = new Command("responsible")
-  .description("Converts Responsible .kdl files to OpenAPI .json")
-  .version(version)
+const program = new Command(packageJSON.name)
+  .description(packageJSON.description)
+  .version(packageJSON.version)
   .helpCommand(true)
   .argument("file", "Responsible .kdl file", x =>
     x.endsWith(".kdl") ? x : undefined,
@@ -52,19 +52,18 @@ const program = new Command("responsible")
           console.log(eventType, filename)
           await onKdlChange(output)(file)
         }
-        return
-      }
-
-      const doc = parseKDL(await fs.readFile(file, "utf8"))
-      if (!doc.output) {
-        return die(`kdl parse errors: ${JSON.stringify(doc.errors, null, 2)}`)
-      }
-
-      const json = JSON.stringify(toOpenAPI(doc.output), null, 2)
-      if (output) {
-        await fs.writeFile(output, json)
       } else {
-        console.log(json)
+        const doc = parseKDL(await fs.readFile(file, "utf8"))
+        if (!doc.output) {
+          return die(`kdl parse errors: ${JSON.stringify(doc.errors, null, 2)}`)
+        }
+
+        const json = JSON.stringify(toOpenAPI(doc.output), null, 2)
+        if (output) {
+          await fs.writeFile(output, json)
+        } else {
+          console.log(json)
+        }
       }
     },
   )
