b8a318e910d58f64f39a7bff43636c58deb4a9a0
adelnizamutdinov <stiggpwnz@gmail.com>
2026-03-29 14:20:16 +0300

    amen

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -2,17 +2,15 @@ import { Command } from "commander"
 import { parse as parseKDL } from "kdljs"
 import * as fs from "node:fs/promises"
 import { toOpenAPI } from "../../generator/src/kdl"
-import * as packageJSON from "../package.json"
+import * as pkgJSON from "../package.json" with { type: "json" }
 
-function die(s: string): never {
-  console.error(s)
-  return process.exit(1)
-}
+const KDL_PARSE_ERRORS = "KDL parse errors:"
 
 const onKdlChange =
   (jsonPath: string) =>
   async (kdlPath: string): Promise<void> => {
-    console.time(`writing ${jsonPath}`)
+    const label = `Writing ${jsonPath}`
+    console.time(label)
 
     const doc = parseKDL(await fs.readFile(kdlPath, "utf8"))
     if (doc.output) {
@@ -20,26 +18,29 @@ const onKdlChange =
         jsonPath,
         JSON.stringify(toOpenAPI(doc.output), null, 2),
       )
-      console.timeEnd(`writing ${jsonPath}`)
+      console.timeEnd(label)
     } else {
-      console.error(JSON.stringify(doc.errors, null, 2))
+      console.error(KDL_PARSE_ERRORS, JSON.stringify(doc.errors, null, 2))
     }
   }
 
-const program = new Command(packageJSON.name)
-  .description(packageJSON.description)
-  .version(packageJSON.version)
+const program = new Command(pkgJSON.name)
+  .description(pkgJSON.description)
+  .version(pkgJSON.version)
   .helpCommand(true)
   .argument("file", "Responsible .kdl file")
   .option("-o, --output <file>", "Output OpenAPI .json file")
-  .option("-w, --watch", "Watch for changes", false)
+  .option("-w, --watch", "Watch for changes")
   .action(
     async (
       file: string,
-      { watch, output }: { watch: boolean; output?: string },
+      { watch, output }: { watch?: boolean; output?: string },
     ) => {
       if (watch) {
-        if (!output) return die("Must specify --output when using --watch")
+        if (!output) {
+          console.error("Must specify --output when using --watch")
+          return
+        }
 
         for await (const { filename, eventType } of fs.watch(file)) {
           if (!filename?.endsWith(".kdl")) continue
@@ -49,7 +50,8 @@ const program = new Command(packageJSON.name)
       } else {
         const doc = parseKDL(await fs.readFile(file, "utf8"))
         if (!doc.output) {
-          return die(`kdl parse errors: ${JSON.stringify(doc.errors, null, 2)}`)
+          console.error(KDL_PARSE_ERRORS, JSON.stringify(doc.errors, null, 2))
+          return
         }
 
         const json = JSON.stringify(toOpenAPI(doc.output), null, 2)
